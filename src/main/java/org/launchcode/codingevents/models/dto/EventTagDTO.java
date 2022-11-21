92aaddca3cb43847b331b188133f74a0283125fe
Emily Hart <emilykhart726@gmail.com>
2022-11-21 12:39:06 -0600

    halfway through many to many

---
+++ src/main/java/org/launchcode/codingevents/models/dto/EventTagDTO.java
@@ -0,0 +1,36 @@
+package org.launchcode.codingevents.models.dto;
+
+import org.launchcode.codingevents.models.Event;
+import org.launchcode.codingevents.models.Tag;
+
+import javax.validation.constraints.NotNull;
+
+// allows us to pass around a tag and an event in a bundle
+
+public class EventTagDTO {
+
+    @NotNull
+    private Event event;
+
+    @NotNull
+    private Tag tag;
+
+    public EventTagDTO() {
+    }
+
+    public Event getEvent() {
+        return event;
+    }
+
+    public void setEvent(Event event) {
+        this.event = event;
+    }
+
+    public Tag getTag() {
+        return tag;
+    }
+
+    public void setTag(Tag tag) {
+        this.tag = tag;
+    }
+}
