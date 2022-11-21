1dba5f5c7bc7b96adb2060662f13acdccbb1d2a2
Emily Hart <emilykhart726@gmail.com>
2022-11-20 21:26:14 -0600

    one to many

--- src/main/java/org/launchcode/codingevents/models/EventCategory.java
+++ src/main/java/org/launchcode/codingevents/models/EventCategory.java
@@ -15,6 +15,10 @@ public class EventCategory extends AbstractEntity {
     @Size(min=3, message="Name must be at least 3 characters long")
     private String name;
 
+    //stores all the events that are in given category, the list itself can't change but the contents can add
+    @OneToMany(mappedBy = "eventCategory") //name of the field in the class we are storing that creates the relationship
+    private final List<Event> events = new ArrayList<>();
+
     public EventCategory(@Size(min = 3, message = "Name must be at least 3 characters long") String name) {
         this.name = name;
     }
@@ -27,6 +31,10 @@ public class EventCategory extends AbstractEntity {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<Event> getEvents() {
+        return events;
     }
 
     @Override
